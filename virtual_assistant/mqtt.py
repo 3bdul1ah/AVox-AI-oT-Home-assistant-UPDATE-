8fa4d97a95f73f3c8b6882d1a9d22b55d653be05
Abdullah Alshateri <141893142+3bdul1ah@users.noreply.github.com>
2024-06-21 09:10:09 -0400

    Update mqtt.py

--- virtual_assistant/mqtt.py
+++ virtual_assistant/mqtt.py
@@ -6,15 +6,13 @@ import sys
 import os
 import re
 from dotenv import load_dotenv
-from deepgram import (
-    DeepgramClient,
-    SpeakOptions,
-)
+from deepgram import DeepgramClient, SpeakOptions
+
 load_dotenv()
 from config import GOOGLE_API_KEY, DG_API_KEY
 import pickle
 import google.generativeai as genai
-
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 MQTT_SERVER = "broker.emqx.io"
 MQTT_PORT = 1883
@@ -22,7 +20,6 @@ MQTT_TOPIC_PUBLISH = "input/voice"
 MQTT_TOPIC_SUBSCRIBE = "Luna/Input"
 
 genai.configure(api_key=GOOGLE_API_KEY)
-
 model = genai.GenerativeModel('gemini-pro')
 
 with open("prompt.json", "r") as f:
@@ -36,31 +33,35 @@ default_prompt = (
     prompt["inferred_responses"]
 )
 
-if(os.path.exists("history")):
+if os.path.exists("history"):
     with open("history", "rb") as fp:
         history = pickle.load(fp)
-
-        chat = model.start_chat(history=history)
+    chat = model.start_chat(history=history)
 else:
     chat = model.start_chat(history=[])
-    response = chat.send_message(default_prompt)
 
+response = chat.send_message(
+    default_prompt,
+    safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+    }
+)
 
 def TTSdeepgram(text, filename):
     try:
-            SPEAK_OPTIONS = {"text": text}
-            deepgram = DeepgramClient(api_key= DG_API_KEY)
-
-            options = SpeakOptions(
-                model="aura-asteria-en",
-                encoding="linear16",
-                container="wav"
-            )
-
-            response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
-
+        SPEAK_OPTIONS = {"text": text}
+        deepgram = DeepgramClient(api_key=DG_API_KEY)
+        options = SpeakOptions(
+            model="aura-asteria-en",
+            encoding="linear16",
+            container="wav"
+        )
+        response = deepgram.speak.v("1").save(filename, SPEAK_OPTIONS, options)
     except Exception as e:
-            print(f"Exception: {e}")
+        print(f"Exception: {e}")
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe(MQTT_TOPIC_SUBSCRIBE)
@@ -74,14 +75,33 @@ def publish_command(message):
 def Luna_Input(client, userdata, msg):
     voice = msg.payload.decode()
     print(voice)
-    response = chat.send_message(voice)
+    try:
+        response = chat.send_message(
+            voice,
+            safety_settings={
+                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
+            }
+        )
+    except:
+        chat.rewind()
+        response = chat.send_message(
+            voice,
+            safety_settings={
+                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
+            }
+        )
     assistant = response.text
     publish_command(assistant)
     print(assistant)
-    TTSdeepgram(assistant,"assistant.wav")
+    TTSdeepgram(assistant, "assistant.wav")
     os.system("aplay -q assistant.wav")
     history = chat.history
-
     with open("history", "wb") as fp:
         pickle.dump(history, fp)
     os.remove("assistant.wav")
@@ -89,6 +109,5 @@ def Luna_Input(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = Luna_Input
-
 client.connect(MQTT_SERVER, MQTT_PORT, 60)
 client.loop_forever()
